ca4b9900c176d4892ad8a36e264219e2673af4ec
robborobcb <robborobcb@gmail.com>
2025-07-11 10:58:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <h2>World!</h2>
   <p>łóśćĄĘŻŹ</p>
 
-
+<?php echo "PHP ...../\---\_____"; ?>
 
   <h2>My First JavaScript</h2>
   <button type="button" onclick="document.getElementById('demo').innerHTML = Date()"> Click me to display Date and Time.</button>
